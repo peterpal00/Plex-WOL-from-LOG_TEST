9dda17fcfa424eceb9841f377aaaabf8275a3742
peterpal00 <ppk.peterpal@gmail.com>
2022-04-26 01:10:32 +0200

    debug lines

--- Plex-WOL-from-LOG/Plex_WOL_from_LOG.py
+++ Plex-WOL-from-LOG/Plex_WOL_from_LOG.py
@@ -4,12 +4,14 @@ from time import sleep
 
 filename = 'Plex Media Server.log'
 filepath = '/path/to/library/Library/Application Support/Plex Media Server/Logs/'
-filee = filepath + filename
+#filee = filepath + filename
 active_users = list()
-#testfile = 'test19.txt'
+testfile = 'test20.txt'
 analyser = LogAnalyser(active_users)
 
 while(True):
-    for line in Pygtail(filee):
+    for line in Pygtail(testfile):
         analyser.check_for_new_user(line)
+        print('newline')
+    print('hello')
     sleep(2)
