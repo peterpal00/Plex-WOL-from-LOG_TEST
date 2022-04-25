7e414020cd2cda0af2dfca447607abc0ebb4dff2
peterpal00 <ppk.peterpal@gmail.com>
2022-04-25 20:21:36 +0200

    ID attribute added to user class

--- Plex-WOL-from-LOG/user.py
+++ Plex-WOL-from-LOG/user.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
 class User:
-    def __init__(self, Address, Name, Login_time) -> None:
+    def __init__(self, Address, ID, Name,) -> None:
         self.address = Address
+        self.id = ID
         self.name = Name
         self.login_time = datetime.now()
 
